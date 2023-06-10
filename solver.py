f0d8a3f74c35d51c9761201c90d9c05a33769962
Sargis <sargishunanyan02@gmail.com>
2023-06-10 14:49:13 +0400

    v 0.7

--- solver.py
+++ solver.py
@@ -1,6 +1,6 @@
+from time import sleep
 from main import board
 n=len(board)
-
 
 class Cell:
     def __init__(self, row: int, col: int, checked=False) -> None:
@@ -24,32 +24,63 @@ class Cell:
                     oc+=1
         return (mines, nc, oc)
     
-    def open_cells(self):
+    def open_cells(self, mines=False):
         for i in range(self.row-2,self.row+1):
             if i<0 or i>= n: continue
             for j in range(self.col-2,self.col+1):
                 if j<0 or j >= n : continue
-                if boardview[i][j].value=='*':
-                    boardview[i][j].click()
+                cell=boardview[i][j]
+                if cell.value=='*':
+                    if not mines:
+                        cell.click()
+                    else:
+                        cell.value=9
+                        cell.checked=True
 
     def click(self):
         if board[self.row-1][self.col-1]==9:
             raise Exception("LOSSEEEEERRRR")#Never
         self.checked=True
         self.value=board[self.row-1][self.col-1]
+        if self.value==0:
+            self.open_cells()
 
     def print(self):
         print(self.value, end=', 'if self.col < n else '\n')
 
 
-
 boardview=[[Cell(i+1,j+1) for j in range(n)] for i in range(n)]
+def print_board():
+    for i in range(n):
+        for j in range(n):
+            boardview[i][j].print()
+    print()
+def solved():
+    lst1=[]
+    for i in boardview:
+        lst1.extend(i)
+    lst2=list(map(str, lst1))
+    return ''.join(lst2).isdigit()
 
+print_board()
+boardview[3][4].open_cells()#########################
+
+while True:
+    for row__index, row in enumerate(boardview):
+        for col_index, col in enumerate(row):
+            cell=boardview[row__index][col_index]
+            if not cell.checked: continue
+            mines, not_checked, checked_nums = cell.look()
+            if not_checked==cell.value-mines or cell.value==mines:
+                cell.open_cells(mines= not_checked==cell.value-mines)
+            
+    print_board()
+    if solved():
+        break
+    sleep(1)
 
 # test
-boardview[3][3].open_cells()
-print(boardview[3][4].row, boardview[3][4].col)
-print(boardview[3][4].look())
-for i in range(n):
-    for j in range(n):
-        boardview[i][j].print()
+# boardview[3][4].open_cells()
+# print_board()
+# print(boardview[3][4].row, boardview[3][4].col)
+# print(boardview[3][4].look())
